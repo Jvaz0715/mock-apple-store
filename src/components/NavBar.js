61a09de2b6d0bd58f8d0ea01f7742212782b9e77
Jvaz0715 <James.Vazquez@codeimmersives.com>
2021-11-07 17:39:26 -0500

    Makes very basic navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import { Box, AppBar, Toolbar } from "@material-ui/core";
+import { Box, AppBar, Toolbar, IconButton } from "@material-ui/core";
+import LocalMallSharpIcon from '@mui/icons-material/LocalMallSharp';
+import { Link } from "react-router-dom";
+import { Typography } from '@mui/material';
 
 function NavBar() {
    return (
@@ -10,13 +13,32 @@ function NavBar() {
                background:"black"
             }}
          >
-            <Toolbar>
-               <img
-                  src="https://www.freepnglogos.com/uploads/apple-logo-png/apple-logo-png-what-you-need-know-before-rebranding-11.png"
-                  alt="apple logo"
-                  width="100px"
-                  height="100px"
-               />
+            <Toolbar
+               style={{
+                  display: "flex",
+               }}
+            >
+               <Link to="/" style={{width: "50%"}}>
+                  <img 
+                     src="https://www.freepnglogos.com/uploads/apple-logo-png/apple-logo-png-what-you-need-know-before-rebranding-11.png"
+                     alt="apple logo"
+                     width="100px"
+                     height="100px"
+                  />
+               </Link>
+               <Typography>Apple Store</Typography>
+               <Link 
+                  to="/bag" 
+                  style={{
+                     width: "50%",
+                     display: "flex",
+                     justifyContent:"end"
+                  }}
+               >
+                  <IconButton size="Large" style={{color: "white"}} edge="end" aria-label="menu">
+                     <LocalMallSharpIcon style={{fontSize:"50px"}} position="end"/>
+                  </IconButton>
+               </Link>
             </Toolbar>
          </AppBar>
       </Box>
