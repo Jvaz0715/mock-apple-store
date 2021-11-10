1a48686f10ddfca7b0a65bec1140b5c0587fb743
Alondradaisy <alondra.warren96@gmail.com>
2021-11-09 20:44:18 -0800

    work through dark/light mode toggle switch component, add material ui dependencies

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect, useReducer} from "react";
+import React, { useState, useEffect, useReducer } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 import { ShoppingBagContext } from "./context/ShoppingBagContext"; //brings in the Shopping Bag Context we created
 import { fetchProducts } from "./ProductsData"; //brings in our mock data
@@ -8,8 +8,9 @@ import { fetchProducts } from "./ProductsData"; //brings in our mock data
 import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
 import ShoppingBag from "./pages/ShoppingBag";
+//import ToggleTheme from "./components/ToggleTheme";
 
-import './App.css';
+import "./App.css";
 
 // we use this object so that we avoid any syntax errors in our dispatch
 export const ACTIONS = {
@@ -22,57 +23,58 @@ export const ACTIONS = {
 function reducer(productsInBag, action) {
   // the add to bag needs to account for multiple selections
   // todo, add products that are same into same object
-  if(action.type === ACTIONS.ADD_TO_BAG) {
-    return [...productsInBag, addNewItem(action.payload)]
+  if (action.type === ACTIONS.ADD_TO_BAG) {
+    return [...productsInBag, addNewItem(action.payload)];
   }
 
-  if(action.type === ACTIONS.DELETE_FROM_BAG) {
-    return productsInBag.filter(product => product.id !== action.payload.id);
+  if (action.type === ACTIONS.DELETE_FROM_BAG) {
+    return productsInBag.filter((product) => product.id !== action.payload.id);
   }
 
-  if(action.type === ACTIONS.EMPTY_BAG) {
-    return productsInBag = [];
+  if (action.type === ACTIONS.EMPTY_BAG) {
+    return (productsInBag = []);
   }
-};
+}
 
 // create a function that will return the item's image, name, and price in an object
-function addNewItem(product){
+function addNewItem(product) {
   return {
     id: uuidv4(), //we give the product an individual title
     title: product.title,
     price: product.price,
     img: product.img,
-  }
+  };
 }
 
 function App() {
-  const bagInLocalStorage = window.localStorage.getItem("shoppingBag")
+  const bagInLocalStorage = window.localStorage.getItem("shoppingBag");
   // check if we already have products in the shopping bag if not set it to empty array
-  const initialShoppingBag = bagInLocalStorage ? JSON.parse(bagInLocalStorage) : [];
+  const initialShoppingBag = bagInLocalStorage
+    ? JSON.parse(bagInLocalStorage)
+    : [];
   const [productsInBag, dispatch] = useReducer(reducer, initialShoppingBag); //this will populate as we add items to our bag
   const [productList, setProductList] = useState([]);
 
   useEffect(() => {
-    fetchProducts()
-      .then((products) =>{
-        setProductList(products)
-      })
-    
-    window.localStorage.setItem('shoppingBag', JSON.stringify(productsInBag))
+    fetchProducts().then((products) => {
+      setProductList(products);
+    });
+
+    window.localStorage.setItem("shoppingBag", JSON.stringify(productsInBag));
   }, [productsInBag]);
 
   // we will pass down this function to the home page so when we add to bag, we update the productsInBag data
-  const addToBag = (product) =>{
-    dispatch({type: ACTIONS.ADD_TO_BAG, payload: product})
+  const addToBag = (product) => {
+    dispatch({ type: ACTIONS.ADD_TO_BAG, payload: product });
   };
 
   const removeFromBag = (product) => {
-    dispatch({type: ACTIONS.DELETE_FROM_BAG, payload: product})
+    dispatch({ type: ACTIONS.DELETE_FROM_BAG, payload: product });
   };
 
   const emptyBag = (productsInBag) => {
-    dispatch({type: ACTIONS.EMPTY_BAG, payload: productsInBag})
-  }
+    dispatch({ type: ACTIONS.EMPTY_BAG, payload: productsInBag });
+  };
 
   const shoppingBagProviderValue = {
     productList,
---
+++ src/components/ToggleTheme.js
@@ -0,0 +1,26 @@
+// import React, { useState } from "react";
+// import Paper from "material-ui/core/Paper";
+// import { createMuiTheme, ThemeProvider } from "@material-ui/styles";
+// import Typography from "material-ui/styles/typography";
+// import Switch from "@material-ui/core//switch"
+
+// const ToggleTheme = () => {
+//   const [darkMode, setDarkMode] = useState(false);
+
+//   const theme = createMuiTheme({
+//       palette: {
+//           type: dark ? 'dark' : 'light',
+//       }.
+//   });
+
+//   return (
+//     <ThemeProvider>
+//         <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)}/>
+//         <Paper>
+//             <Typography variant="body">BODY</Typography>
+//         </Paper>
+//     </ThemeProvider>
+//   );
+// };
+
+// export default ToggleTheme;
